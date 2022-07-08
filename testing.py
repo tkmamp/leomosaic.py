546a773a9fdc8d1b98ff0a81c3ed89b297c5fce6
kamp-to <tobias.kamp@dlr.de>
2022-07-08 21:50:29 +0200

    minor fix

--- testing.py
+++ testing.py
@@ -10,6 +10,6 @@ w_tiles = 70
 h_tiles = w_tiles #int(w_tiles*3/4)
 mymosaic.create_image_mosaic(target_im, source_dir, reuse_images=False, max_reuse=-10, 
                              target_resolution_wh=(w, h), target_number_tiles_wh=(w_tiles, h_tiles), 
-                             overlay=0.5, kshape=(7,7), desaturate=0.8, enhance_target=1.2)
+                             overlay=0.44, kshape=(7,7), desaturate_tiles=0.8, enhance_target=1.2)
 mymosaic.blended_mosaic.save(fp="C:\\Users\\Tobia\\Desktop\\demo.png") # save to this file
 print("ready")
